23919add1f533afe7c1902951b7ff17ec0de0600
Joshua Hamill <joshuahamill@Joshuas-MacBook-Pro.local>
2025-06-12 10:08:54 -0400

    add coffee to sketchybar

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,7 +1,15 @@
 local hyper = { "ctrl", "alt", "cmd", "shift" }
 hs.application.enableSpotlightForNameSearches(true)
 
--- Enable caffeine
+caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+	if state then
+		caffeine:setTitle("󰅶")
+	else
+		caffeine:setTitle("󰛊")
+	end
+end
+
 function caffeineClicked()
 	setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
 end
