68041a724a9cc11ec032a6875da5d95170b33f17
Joshua Hamill <joshuahamill@Joshuas-MacBook-Pro-2.local>
2025-06-25 12:44:52 -0400

    enable telescope to search hidden files

--- .config/nvim/lua/kickstart/plugins/base.lua
+++ .config/nvim/lua/kickstart/plugins/base.lua
@@ -190,6 +190,13 @@ return {
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
+			vim.keymap.set("n", "<leader>sa", function()
+				builtin.find_files({
+					hidden = true,
+					no_ignore = true,
+				})
+			end, { desc = "[S]earch [A]ll Files" })
+
 			-- Slightly advanced example of overriding default behavior and theme
 			vim.keymap.set("n", "<leader>/", function()
 				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
