584a0bd8ab4eb3b19666dda2dde5f1c775c58418
Josh Hamill <joshrasmussen34@gmail.com>
2025-11-11 11:05:14 -0500

    feat: update neovim snacks dashboard

--- stow/dot-config/nvim/lua/custom/plugins/snacks.lua
+++ stow/dot-config/nvim/lua/custom/plugins/snacks.lua
@@ -1,3 +1,41 @@
+-- Generate ASCII Art here -> https://patorjk.com/software/taag/
+local dclm_dev = [[
+██████╗  ██████╗██╗     ███╗   ███╗    ██████╗ ███████╗██╗   ██╗
+██╔══██╗██╔════╝██║     ████╗ ████║    ██╔══██╗██╔════╝██║   ██║
+██║  ██║██║     ██║     ██╔████╔██║    ██║  ██║█████╗  ██║   ██║
+██║  ██║██║     ██║     ██║╚██╔╝██║    ██║  ██║██╔══╝  ╚██╗ ██╔╝
+██████╔╝╚██████╗███████╗██║ ╚═╝ ██║    ██████╔╝███████╗ ╚████╔╝ 
+╚═════╝  ╚═════╝╚══════╝╚═╝     ╚═╝    ╚═════╝ ╚══════╝  ╚═══╝  ]]
+
+local mickey = [[
+ █████╗   █████╗ 
+███████╗ ███████╗
+███████║ ███████║
+ █████╔╝  █████╔╝
+ ╚═██████████══╝ 
+  ████████████╗  
+  ████████████║  
+  ████████████║  
+    ████████╔═╝  
+    ╚═══════╝    ]]
+
+local jhamill = [[
+     ██╗██╗  ██╗ █████╗ ███╗   ███╗██╗██╗     ██╗     
+     ██║██║  ██║██╔══██╗████╗ ████║██║██║     ██║     
+     ██║███████║███████║██╔████╔██║██║██║     ██║     
+██   ██║██╔══██║██╔══██║██║╚██╔╝██║██║██║     ██║     
+╚█████╔╝██║  ██║██║  ██║██║ ╚═╝ ██║██║███████╗███████╗
+ ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚══════╝╚══════╝ ]]
+
+local function get_header()
+	local user = vim.fn.expand("$USER")
+	if user == "josh.hamill.-nd" then
+		return mickey .. "\n\n" .. dclm_dev
+	end
+
+	return jhamill
+end
+
 return {
 	{
 		"folke/snacks.nvim",
@@ -8,7 +46,67 @@ return {
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
-			dashboard = { example = "files" },
+			dashboard = {
+				preset = {
+					header = get_header(),
+				},
+				sections = {
+					{ section = "header" },
+					{ section = "keys", gap = 1, padding = 1 },
+					{
+						pane = 2,
+						icon = " ",
+						desc = "Browse Repo",
+						padding = 1,
+						key = "b",
+						action = function()
+							Snacks.gitbrowse()
+						end,
+					},
+					function()
+						local in_git = Snacks.git.get_root() ~= nil
+						local cmds = {
+							{
+								title = "Open Issues",
+								cmd = "gh issue list -L 3",
+								key = "i",
+								action = function()
+									vim.fn.jobstart("gh issue list --web", { detach = true })
+								end,
+								icon = " ",
+								height = 7,
+							},
+							{
+								icon = " ",
+								title = "Open PRs",
+								cmd = "gh pr list -L 3",
+								key = "P",
+								action = function()
+									vim.fn.jobstart("gh pr list --web", { detach = true })
+								end,
+								height = 7,
+							},
+							{
+								icon = " ",
+								title = "Git Status",
+								cmd = "jj st --no-pager",
+								height = 10,
+							},
+						}
+						return vim.tbl_map(function(cmd)
+							return vim.tbl_extend("force", {
+								pane = 2,
+								section = "terminal",
+								enabled = in_git,
+								padding = 1,
+								ttl = 5 * 60,
+								indent = 3,
+							}, cmd)
+						end, cmds)
+					end,
+					{ section = "startup" },
+				},
+			},
 			explorer = { enabled = true },
 			image = {
 				enabled = true,
